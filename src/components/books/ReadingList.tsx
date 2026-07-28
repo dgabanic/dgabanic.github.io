08971c1076070c15c962b53b1986524d7fbc9ad5
dgabanic <dgabanic@gmail.com>
2026-07-28 16:45:53 -0400

    update reading list

--- src/components/books/ReadingList.tsx
+++ src/components/books/ReadingList.tsx
@@ -65,6 +65,7 @@ const BOOKS: Book[] = [
   { date: 'June 2026', title: 'Dark Matter', author: 'Blake Crouch' },
   { date: 'June 2026', title: 'Brilliant Orange', author: 'David Winner' },
   { date: 'July 2026', title: 'The Well of Ascension', author: 'Brandon Sanderson' },
+  { date: 'July 2026', title: 'Angel Down', author: 'Daniel Kraus' },
 ];
 
 const currentlyReading = BOOKS[BOOKS.length - 1];
